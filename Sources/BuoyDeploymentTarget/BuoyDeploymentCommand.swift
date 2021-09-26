c3e7ec34dfe0dfdb061e2b13d54ff9916a7195bd
Florian Lercher <florian.lercher@tum.de>
2021-09-26 13:25:55 +0000

    Register post discovery actions

--- Sources/BuoyDeploymentTarget/BuoyDeploymentCommand.swift
+++ Sources/BuoyDeploymentTarget/BuoyDeploymentCommand.swift
@@ -1,4 +1,5 @@
 import ArgumentParser
+import BuoyDeploymentOption
 import DeploymentTargetIoTCommon
 import DeploymentTargetIoT
 import DeviceDiscovery
@@ -21,21 +22,37 @@ struct BuoyDeployCommand: ParsableCommand {
     var deploymentOptions: IoTDeploymentOptions
     
     func run() throws {
-        // let provider = IoTDeploymentProvider(
-        //     searchableTypes: deploymentOptions.types.split(separator: ",").map(String.init),
-        //     productName: deploymentOptions.productName,
-        //     packageRootDir: deploymentOptions.inputPackageDir,
-        //     deploymentDir: deploymentOptions.deploymentDir,
-        //     automaticRedeployment: deploymentOptions.automaticRedeploy,
-        //     additionalConfiguration: [
-        //         .deploymentDirectory: deploymentOptions.deploymentDir
-        //     ],
-        //     webServiceArguments: webServiceArguments,
-        //     //            input: .package
-        //     input: .dockerImage("hendesi/master-thesis:latest-arm64")
-        // )
-        // provider.registerAction(scope: .all, action: .action(LIFXDeviceDiscoveryAction.self), option: DeploymentDeviceMetadata(.lifx))
-        // try provider.run()
-        print("Deployed")
+        let provider = IoTDeploymentProvider(
+            searchableTypes: deploymentOptions.types.split(separator: ",").map(String.init),
+            productName: deploymentOptions.productName,
+            packageRootDir: deploymentOptions.inputPackageDir,
+            deploymentDir: deploymentOptions.deploymentDir,
+            automaticRedeployment: deploymentOptions.automaticRedeploy,
+            webServiceArguments: webServiceArguments,
+            input: .dockerImage("hendesi/master-thesis:latest-arm64") // docker compose
+        )
+        registerSensorPostDiscovery(provider, device: .temperature, sensorType: 0, actionName: "temperature")
+        registerSensorPostDiscovery(provider, device: .conductivity, sensorType: 1, actionName: "conductivity")
+        registerSensorPostDiscovery(provider, device: .ph, sensorType: 2, actionName: "ph")
+        try provider.run()
+    }
+
+    func registerSensorPostDiscovery(_ provider: IoTDeploymentProvider, device: DeploymentDevice, sensorType: Int, actionName: String) {
+        provider.registerAction(
+            scope: .all, 
+            action: .docker(
+                        DockerDiscoveryAction(
+                            identifier: ActionIdentifier(actionName),
+                            imageName: "ghcr.io/fa21-collaborative-drone-interactions/buoypostdiscovery:latest",
+                            fileUrl: URL(fileURLWithPath: deploymentOptions.deploymentDir).appendingPathComponent(actionName),
+                            options: [
+                                .command("\(sensorType) \(actionName)"),
+                                .volume(hostDir: "/buoy", containerDir: "/buoy"),
+                                .volume(hostDir: deploymentOptions.deploymentDir, containerDir: "/result")
+                            ]
+                        )
+                    ),
+            option: DeploymentDeviceMetadata(device)
+        )
     }
 }
