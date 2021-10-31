71de6c76e8fc8c64b9e2511cf3f875996cb33890
Felix Desiderato <felix.desiderato@tum.de>
2021-10-31 20:15:04 +0100

    Update script

--- scripts/buoy_simulation.sh
+++ scripts/buoy_simulation.sh
@@ -4,6 +4,7 @@ DEFAULT='\033[0m'
 EXEC_PATH=".build/debug/BuoyDeploymentTarget --config-file config.json --docker-compose-path buoy-deployment-provider/docker-compose.yml"
 
 ipAddresses=( "192.168.2.116" "192.168.2.118" "192.168.2.119" )
+sensorNames=( "temperature" "conductivity" "ph" )
 
 function reset() {
     for ipAddress in "${ipAddresses[@]}"; do
@@ -17,34 +18,53 @@ ssh ubuntu@${ipAddresses[0]} 'echo "[0,2]" >| /buoy/available_sensors.json'
 ssh ubuntu@${ipAddresses[1]} 'echo "[0,1]" >| /buoy/available_sensors.json'
 ssh ubuntu@${ipAddresses[2]} 'echo "[2]" >| /buoy/available_sensors.json'
 
-echo "Testing normal deployment. Downloading images only on first run"
-reset
-for ((i=1;i<=10;i++)); do
-    SECONDS=0
-    ./$EXEC_PATH
-    echo "$i - $SECONDS"$'\n' >> buoy_resultTimes_normal.txt
-    echo "${GREEN}\xE2\x9C\x94 RUN $i done in $SECONDS${DEFAULT}"
-done
+function call() {
+    currentCall=$1:80/v1/data/${sensorNames[$2]}
+    call=$(curl -s -o /dev/null -w "%{http_code}" $currentCall)
+    if [ $call -eq 200 ] && $3; then
+        echo "${GREEN}\xE2\x9C\x94 SUCCESS${DEFAULT}: $currentCall was successful"
+    elif [ $call -ne 200 ] && ! $3; then
+        echo "${GREEN}\xE2\x9C\x94 SUCCESS${DEFAULT}: $currentCall was expected to fail."
+    else
+        echo "${RED}ERROR${DEFAULT}: $currentCall failed unexpected."
+        errorOccurred=true
+    fi
+}
+
+function evaluate() {
+    call ${ipAddresses[0]} 0 true
+    call ${ipAddresses[0]} 1 false
+    call ${ipAddresses[0]} 2 true
+
+    call ${ipAddresses[1]} 0 true
+    call ${ipAddresses[1]} 1 true
+    call ${ipAddresses[1]} 2 false
+
+    call ${ipAddresses[2]} 0 false
+    call ${ipAddresses[2]} 1 false
+    call ${ipAddresses[2]} 2 true
+}
 
 sleep 120
-echo "Testing with docker reset. Downloading images on every run"
+echo "Testing normal deployment. Downloading images only on first run"
 
 for ((i=1;i<=10;i++)); do
     reset
-    SECONDS=0
     ./$EXEC_PATH
-    echo "$i - $SECONDS"$'\n' >> buoy_resultTimes_reset.txt
-    echo "${GREEN}\xE2\x9C\x94 RUN $i done in $SECONDS${DEFAULT}"
+    evaluate
 done
 
 sleep 120
 echo "Testing without docker reset. Assuming needed images are already downloaded"
 
 for ((i=1;i<=10;i++)); do
-    SECONDS=0
     ./$EXEC_PATH
-    echo "$i - $SECONDS"$'\n' >> buoy_resultTimes_noReset.txt
-    echo "${GREEN}\xE2\x9C\x94 RUN $i done in $SECONDS${DEFAULT}"
+    evaluate
 done
 
+    if [ "$errorOccurred" = false ]; then
+        echo "${GREEN}\xE2\x9C\x94 Run was successful!${DEFAULT}"
+    else
+        echo "${RED}Run failed!${DEFAULT}"
+    fi
 
